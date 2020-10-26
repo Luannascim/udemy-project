93dbc7dceae1ec25cc7d875e156806d0b2a4a823
luan <luan14075@gmail.com>
2020-10-26 15:13:22 -0300

    primeiro commit

---
+++ src/test/java/br/com/udemyproject/UdemyProject/UdemyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.udemyproject.UdemyProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
